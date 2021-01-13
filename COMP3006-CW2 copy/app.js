708655c56a9d34dc22898efcb9c0a848bceccf8d
jhicks5 <joshua.hicks@students.plymouth.ac.uk>
2021-01-13 17:19:48 +0000

    Added comments to app.js and db.js

--- COMP3006-CW2 copy/app.js
+++ COMP3006-CW2 copy/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+// parsers for data provided from html user
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 const path = require('path');
@@ -12,57 +13,72 @@ const db = require('./db');
 const { json } = require('express');
 const collection = 'questions';
 
+// serve the html file to the user
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// function to test a user connecting
 server.on('connection', function(socket){
     console.log("A user connected");
 })
 
+// read all documents from database
 app.get('/getQuestions',(req,res)=>{
     db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        // catch errors
         if(err)
             console.log(err);
         else{
+            // reverse documents so last entry shown first
             documents.reverse();
-            console.log(documents);
+            // return to user as json
             res.json(documents);
         }
     })
 });
 
+// update documents
 app.put('/:id',(req,res)=>{
+    // key of question to be updated
     const questionID = req.params.id;
+    // variable passed by html on which vote chosen
     const voteType = req.body;
-    console.log('vote is', voteType)
+    // find document by ID, increment vote by one
     db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(questionID)},
     {$inc : {[voteType] : 1}},{returnOriginal : false},(err,result)=>{
         if(err)
             console.log(err);
         else{
+            // return update to user as json
             res.json(result);
         }
     })
 });
 
+// create document
 app.post('/',(req,res)=>{
+    // document to be inserted
     const userInput = req.body;
-    console.log(userInput);
+    // insert document
     db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
         if(err)
             console.log(err);
         else
+            // return document to user
             res.json({result : result, document : result.ops[0]});
     });
 });
 
 app.delete('/id',(req,res)=>{
+    // id of document to be deleted
     const questionID = req.params.id;
+    // use findoneanddelete function
     db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(questionID)},(err,result)=>{
         if(err)
             console.log(err);
         else
+            //return response to user
             res.json(result);
     });
 });
@@ -79,8 +95,10 @@ app.delete('/id',(req,res)=>{
     }
 }); */
 
+// initialise socket
 http.listen(3000, function(){
     console.log('listening on port 3000')
+    // connect to database
     db.connect((err)=>{
         if(err){
             console.log('Unable to connect to database');
--- COMP3006-CW2 copy/db.js
+++ COMP3006-CW2 copy/db.js
@@ -1,6 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+// Database name
 const dbname = 'cw2-database';
+// Database URL
 const uri = "mongodb+srv://JHicks:CW2Pass@cluster0.r0mhr.mongodb.net/cw2-database?retryWrites=true&w=majority";
 const mongoOptions = {useNewUrlParser : true};
 
@@ -9,12 +11,16 @@ const state = {
 };
 
 const connect = (cb) =>{
+    // if we already have a connection, call cb
     if(state.db)
         cb();
     else{
+        // Attempt connention
         MongoClient.connect(uri,mongoOptions,(err,client)=>{
+            // If unable to connect pass error to cb
             if(err)
                 cb(err);
+            // If successful, set db state and call cb
             else{
                 state.db = client.db(dbname);
                 cb();
@@ -23,10 +29,12 @@ const connect = (cb) =>{
     }
 }
 
+// returns the object id
 const getPrimaryKey = (_id)=>{
     return ObjectID(_id);
 }
 
+// returns database connection
 const getDB = ()=>{
     return state.db;
 }
